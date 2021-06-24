aeac222c0653ded5c11653b448feb7a2e9e70c78
alex <arodriguez202020@gmail.com>
2021-06-24 08:42:29 -0500

    Add migration files for director and english to movies and gender and age to actors with updated controller methods

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,5 +15,5 @@
 # actor = Actor.new({first_name: "Tom", last_name: "Cruise", known_for: "Top Gun"})
 # actor.save
 
-movie = Movie.new({title: "The Dark Knight", year: 2008, plot: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice."})
-movie.save
+# movie = Movie.new({title: "The Dark Knight", year: 2008, plot: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice."})
+# movie.save
