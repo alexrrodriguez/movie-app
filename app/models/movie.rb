72afdbb1fceeccad2517e08aefea68f051ac671c
alex <arodriguez202020@gmail.com>
2021-06-29 09:19:32 -0500

    add movie_id to actor model and create has_many and belongs_to actions, add actor serializer with belongs_to movie and update create and update actions

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,6 @@ class Movie < ApplicationRecord
   validates :title, presence: true
   validates :year, numericality: { greater_than: 1888 }
   validates :plot, presence: true
+
+  has_many :actors
 end
