f1949c1544d15af6dba1865bf86ba27701b81e4c
alex <arodriguez202020@gmail.com>
2021-07-21 20:30:05 -0500

    update movies controller authenticate_admin exceptions to only allow admins to create, update, and destroy

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-  before_action :authenticate_admin, except: [:index, :show, :create, :update, :destroy]
+  before_action :authenticate_admin, except: [:index, :show,]
 
   def index
     movie = Movie.all
